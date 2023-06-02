f67405357011290f62b16653c233f3d9f8c6646a
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2023-06-03 03:33:04 +0530

    modified about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -42,44 +42,44 @@ export default function About(props) {
                     <h2 className="featurette-heading fw-normal lh-1" style={{ color: props.Mode === "dark" ? "white" : "black" }}>Hello, it’s that good. <span className={`text-body-${props.Mode}`}>See for yourself.</span></h2>
                     <p style={{ color: props.Mode === "dark" ? "white" : "black" }} className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ut fuga ab suscipit inventore. Rerum modi cumque, nobis corporis necessitatibus architecto, quia sunt autem ab laudantium illum et illo inventore!</p>
                 </div>
-                {images.map((image, index) => (
-                    <div key={index} className="col-md-3 order-md-1">
-                        {image && <img className="img-fluid" style={{ maxWidth: "100%" }} src={image} alt="" />}
-                    </div>
-                ))}
+
+                <div className="col-md-3 order-md-1">
+                    {images[0] && <img className="img-fluid" style={{ maxWidth: "100%" }} src={images[0]} alt="" />}
+                </div>
+
             </div>
             <div className="row featurette d-flex justify-content-center align-items-center my-3">
                 <div className="col-md-6 order-md-1">
                     <h2 className="featurette-heading fw-normal lh-1" style={{ color: props.Mode === "dark" ? "white" : "black" }}>Hurray, it’s that good. <span className={`text-body-${props.Mode}`}>See for yourself.</span></h2>
                     <p style={{ color: props.Mode === "dark" ? "white" : "black" }} className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos eum assumenda illo qui! Excepturi, animi at corrupti necessitatibus deleniti nostrum nemo quae eveniet tenetur, similique vitae culpa, recusandae ex rem!</p>
                 </div>
-                {images.map((image, index) => (
-                    <div key={index} className="col-md-3 order-md-2">
-                        {image && <img className="img-fluid" style={{ maxWidth: "100%" }} src={image} alt="" />}
-                    </div>
-                ))}
+
+                <div className="col-md-3 order-md-2">
+                    {images[1] && <img className="img-fluid" style={{ maxWidth: "100%" }} src={images[1]} alt="" />}
+                </div>
+
             </div>
             <div className="row featurette d-flex justify-content-center align-items-center my-3">
                 <div className="col-md-6 order-md-2">
                     <h2 className="featurette-heading fw-normal lh-1" style={{ color: props.Mode === "dark" ? "white" : "black" }}>Hey, it’s that good. <span className={`text-body-${props.Mode}`}>See for yourself.</span></h2>
                     <p style={{ color: props.Mode === "dark" ? "white" : "black" }} className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iure vel, minus magnam magni facilis eos voluptates ex sed ducimus beatae quaerat aliquam harum pariatur? Quisquam et reprehenderit soluta ex.</p>
                 </div>
-                {images.map((image, index) => (
-                    <div key={index} className="col-md-3 order-md-1">
-                        {image && <img className="img-fluid" style={{ maxWidth: "100%" }} src={image} alt="" />}
-                    </div>
-                ))}
+
+                <div className="col-md-3 order-md-1">
+                    {images[2] && <img className="img-fluid" style={{ maxWidth: "100%" }} src={images[2]} alt="" />}
+                </div>
+
             </div>
             <div className="row featurette d-flex justify-content-center align-items-center my-3">
                 <div className="col-md-6 order-md-1">
                     <h2 className="featurette-heading fw-normal lh-1" style={{ color: props.Mode === "dark" ? "white" : "black" }}>Oh yeah, it’s that good. <span className={`text-body-${props.Mode}`}>See for yourself.</span></h2>
                     <p style={{ color: props.Mode === "dark" ? "white" : "black" }} className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quisquam pariatur odio, reiciendis nostrum, facere rerum provident quae, dolore assumenda facilis laboriosam ratione! Ea ducimus tempore explicabo delectus fugit accusantium.</p>
                 </div>
-                {images.map((image, index) => (
-                    <div key={index} className="col-md-3 order-md-2">
-                        {image && <img className="img-fluid" style={{ maxWidth: "100%" }} src={image} alt="" />}
-                    </div>
-                ))}
+
+                <div className="col-md-3 order-md-2">
+                    {images[3] && <img className="img-fluid" style={{ maxWidth: "100%" }} src={images[3]} alt="" />}
+                </div>
+
             </div>
         </div>
     );
